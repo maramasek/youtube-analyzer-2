5122458c435b58105a3cfbcc405508c926380e00
maramasek <marekmasek@gmail.com>
2025-06-25 15:45:03 +0200

    Delete script.py

--- script.py
+++
@@ -1,9 +0,0 @@
-# Nejdříve si přečtu existující HTML soubor, abych pochopil jeho strukturu
-with open("index.html", "r", encoding="utf-8") as f:
-    html_content = f.read()
-
-# Zobrazím první část HTML souboru pro pochopení struktury
-print("První část HTML souboru:")
-print(html_content[:1000])
-print("...")
-print("Celková délka HTML souboru:", len(html_content))
